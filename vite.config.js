4803085d10f7a5824f777704792de259cae49701
jeevabha <jeevabharathi@gmail.com>
2024-03-17 09:38:38 +0530

    Final

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:"/Chat_App/",
+  base:"/Chat_App_vite/",
   plugins: [react()],
 })
